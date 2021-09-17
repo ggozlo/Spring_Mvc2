9052600c3a0db8776302d17bf41875091925f52f
ggozlo <ggozlo@naver.com>
2021-09-17 23:37:28 +0900

    exception fin

--- exception/src/main/java/hello/exception/ExceptionApplication.java
+++ exception/src/main/java/hello/exception/ExceptionApplication.java
@@ -2,6 +2,8 @@ package hello.exception;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.filter.CharacterEncodingFilter;
 
 @SpringBootApplication
 public class ExceptionApplication {
@@ -10,4 +12,13 @@ public class ExceptionApplication {
 		SpringApplication.run(ExceptionApplication.class, args);
 	}
 
+
+
+	@Bean
+	public CharacterEncodingFilter characterEncodingFilter() {
+		CharacterEncodingFilter characterEncodingFilter = new CharacterEncodingFilter();
+		characterEncodingFilter.setEncoding("UTF-8");
+		characterEncodingFilter.setForceEncoding(true);
+		return characterEncodingFilter;
+	}
 }
