1c6feccb99c1fd72030cfbe1efbdad06d87c2667
ggozlo <ggozlo@naver.com>
2021-09-19 02:49:21 +0900

    typeConverter fin

---
+++ typeconverter/src/test/java/hello/typeconverter/converter/ConverterTest.java
@@ -0,0 +1,44 @@
+package hello.typeconverter.converter;
+
+import hello.typeconverter.type.IpPort;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class ConverterTest {
+
+    @Test
+    public void stringToInteger() {
+
+        StringToIntegerConverter converter = new StringToIntegerConverter();
+        Integer result = converter.convert("10");
+        assertThat(result).isEqualTo(10);
+
+    }
+
+    @Test
+    public void integerToString() {
+
+        IntegerToStringConverter converter = new IntegerToStringConverter();
+        String result = converter.convert(10);
+        assertThat(result).isEqualTo("10");
+    }
+
+    @Test
+    public void stringToIpPort() {
+
+        IpPortToStringConverter converter = new IpPortToStringConverter();
+        IpPort source = new IpPort("127.0.0.0", 8080);
+        String result = converter.convert(source);
+        assertThat(result).isEqualTo("127.0.0.0:8080");
+    }
+    
+    @Test
+    public void iIpPortToString() {
+        StringToIpPortConverter converter = new StringToIpPortConverter();
+        String source = "127.0.0.0:8080";
+        IpPort result = converter.convert(source);
+        assertThat(result).isEqualTo(new IpPort("127.0.0.0", 8080));
+    }
+}
